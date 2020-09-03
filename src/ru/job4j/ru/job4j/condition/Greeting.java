0c249d6ff58d400f507a8bb8366e9ecc7eead082
study4work <FaJlJleN@ya.ru>
2020-09-03 22:14:28 +0300

    9.1. String

---
+++ src/ru/job4j/ru/job4j/condition/Greeting.java
@@ -0,0 +1,12 @@
+package ru.job4j.ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java! ";
+        System.out.println(idea);
+        idea = idea + "But i'm a newbie ";
+        int year = 2020;
+        idea = idea + year;
+        System.out.println(idea);
+    }
+}
