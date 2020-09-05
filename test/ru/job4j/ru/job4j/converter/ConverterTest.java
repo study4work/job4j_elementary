3ff8732f16225a5aad9e3ec8caf63ab9d7e639cf
study4work <FaJlJleN@ya.ru>
2020-09-05 22:18:12 +0300

    0. Что такое тестирование

---
+++ test/ru/job4j/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.ru.job4j.converter;
+
+import org.junit.Test;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ConverterTest {
+
+    @Test
+    public void rubleToEuro() {
+        int in = 140;
+        int expected = 2;
+        int out = Converter.rubleToEuro(in);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void rubleToDollar() {
+        double in = 140;
+        double expected = 2.3333333333333335;
+        double out = Converter.rubleToDollar(in);
+        Assert.assertEquals(expected, out, 0.01);
+
+    }
+}
