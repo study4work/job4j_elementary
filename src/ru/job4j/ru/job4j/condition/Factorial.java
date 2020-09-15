94e0479842407f1d0fbb0bdcd3b3ed537fe55c52
study4work <FaJlJleN@ya.ru>
2020-09-15 20:53:33 +0300

    5.2. Создать программу, вычисляющую факториал.

--- src/ru/job4j/ru/job4j/condition/Factorial.java
+++ src/ru/job4j/ru/job4j/condition/Factorial.java
@@ -3,8 +3,8 @@ package ru.job4j.ru.job4j.condition;
 public class Factorial {
         public static int calc(int n) {
             int result = 1;
-            for (int i = 0; i < n; i++) {
-                result = result * (i + 1);
+            for (int i = 2; i <= n; i++) {
+                result = result * i;
             }
             return result;
         }
