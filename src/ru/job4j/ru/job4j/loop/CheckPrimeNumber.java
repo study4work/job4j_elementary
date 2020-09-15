f6b07821763f68799b58930f0d034a8e46800168
study4work <FaJlJleN@ya.ru>
2020-09-15 18:56:08 +0300

    5.5. Простое число

--- src/ru/job4j/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,13 +2,11 @@ package ru.job4j.ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = false;
-        for (int i = 2; i <= number - 1; i++) {
+        boolean prime = number > 1;
+        for (int i = 2; i < number; i++) {
             if (number % i == 0) {
                 prime = false;
                 break;
-            } else {
-                prime = true;
             }
         }
 
