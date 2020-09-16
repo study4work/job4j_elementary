27fa8607731edf3bc6ff705807d22bc23da6fa3a
study4work <FaJlJleN@ya.ru>
2020-09-16 19:17:33 +0300

    5.9.1. Генерация конструкций в IDEA

---
+++ src/ru/job4j/ru/job4j/loop/Loop.java
@@ -0,0 +1,9 @@
+package ru.job4j.ru.job4j.loop;
+
+public class Loop {
+    public static void main(String[] args) {  /* psvm */
+        for (int i = 0; i < 10; i++) {        /* fori */
+            System.out.println();             /* sout*/
+        }
+    }
+}
