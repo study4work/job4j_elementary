7a7fca45e80049e19f1627c19dfc1dc73b95de74
study4work <FaJlJleN@ya.ru>
2020-09-17 19:24:43 +0300

    6.0.2. Заполнение массива.

--- src/ru/job4j/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,15 @@ public class ArrayDefinition {
         System.out.println("Размер массива ages равен: " + ages.length);
         System.out.println("Размер массива surnames равен: " + surnames.length);
         System.out.println("Размер массива prices равен: " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Ivanov Viktor";
+        names[1] = "Ivanov Vladimir";
+        names[2] = "Ivanov Ivan";
+        names[3] = "Ivanov Andrey";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
