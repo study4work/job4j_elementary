f87d14e1a1974d840f4811191b258520aa1b2eda
study4work <FaJlJleN@ya.ru>
2020-09-26 23:30:47 +0300

    6.7.3. Массив из диагонали матрицы.

--- src/ru/job4j/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/ru/job4j/array/MatrixCheck.java
@@ -3,14 +3,20 @@ package ru.job4j.ru.job4j.array;
 public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
-        for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
                 if (board[row][j] != 'X') {
                     result = false;
                     break;
                 }
             }
-        }
         return result;
+    }
+
+    public static char[] extractDiagonal(char[][] board) {
+        char[] rsl = new char[board.length];
+        for (int i = 0; i < board.length; i++) {
+            rsl[i] = board[i][i];
+        }
+        return rsl;
     }
 }
